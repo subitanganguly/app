25c6989e6a0641c1cdc9dae1fdb1e8da797e75b6
Subitan Ganguly <101342918+subitanganguly@users.noreply.github.com>
2026-02-23 20:10:26 +0530

    Add files via upload

---
+++ js/firebaseconfig.js
@@ -0,0 +1,9 @@
+  // 🔥 Your Firebase Config
+  const firebaseConfig = {
+    apiKey: "AIzaSyBHB-QUTTeCB9TAtc_x-3beCrWYk5GtuQA",
+    authDomain: "test-barman.firebaseapp.com",
+    databaseURL: "https://test-barman-default-rtdb.firebaseio.com",
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
